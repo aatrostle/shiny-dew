67518ef77ccec23082d229327a008ca680680850
Aaron Trostle <aaron.trostle@gmail.com>
2017-11-14 13:51:15 -0800

    Add react and react-dom to dependencies, update index.js to render a react component.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const mountNode = document.getElementById('root');
+
+class HelloMessage extends React.Component {
+  render() {
+    return React.createElement("div", null, "Hello ", this.props.name);
+  }
+}
+
+ReactDOM.render(React.createElement(HelloMessage, { name: "Jane" }), mountNode);
